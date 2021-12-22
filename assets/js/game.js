92b7119eab5d7918de890e7fffb0d6fd65dc72b5
Laertes <maclellanj15@gmail.com>
2021-12-22 18:21:26 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,74 +17,67 @@ var fight = function(enemyName) {
     // repeat and execute as long as the enemy robot is alive
     while(playerHealth > 0 && enemyHealth > 0)  {
     
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-    // if the player choses to skip
-    if (promptFight === "skip" || promptFight === "SKIP") {
-        //confirm player wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to skip?");
+        // if the player choses to skip
+        if (promptFight === "skip" || promptFight === "SKIP") {
+            //confirm player wants to skip
+            var confirmSkip = window.confirm("Are you sure you'd like to skip?");
 
-        // if yes (true), leave fight
-        if (confirmSkip) {
-            window.alert(playerName + " has decided to skip this fight. Goodbye!");
-            // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
-            console.log("playerMoney", playerMoney);
-            break;
+            // if yes (true), leave fight
+            if (confirmSkip) {
+                window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                // subtract money from playerMoney for skipping
+                playerMoney = Math.max(0, playerMoney - 10);
+                console.log("playerMoney", playerMoney);
+                break;
+            }
         }
-    }
-    // console.log(promptFight);
+        // console.log(promptFight);
 
-    //if player choses to fight, then fight
-    // if (promptFight === "fight" || promptFight === "FIGHT") {
+        //if player choses to fight, then fight
+        // if (promptFight === "fight" || promptFight === "FIGHT") {
 
-    // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
-    enemyHealth = enemyHealth - playerAttack;
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
 
-    // Log a resulting message to the console so we know that it worked.
-    console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-    );
+        enemyHealth = Math.max(0, enemyHealth - damage);
+        // Log a resulting message to the console so we know that it worked.
+        console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        );
 
-    //check enemy's health
-    if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
+        //check enemy's health
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
 
-        // award player money for winning 
-        playerMoney = playerMoney + 20;
+            // award player money for winning 
+            playerMoney = playerMoney + 20;
 
-        // leave while() loop since enemy is dead
-        break;
-    } else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
-    }
+            // leave while() loop since enemy is dead
+            break;
+        } else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
 
-    // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
-    playerHealth = playerHealth - enemyAttack;
+        // generate random damage based on enemy's attack power
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
 
-    // Log a resulting message to the console so we know that it worked. 
-    console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
-    
-    // check player's health
-    if (playerHealth <= 0) {
-        window.alert(playerName + " has died!");
-        // leave while() loop if player is dead 
-        break;
-    } else {
-        window.alert(playerName + " still has " + playerHealth + " health left.");
-    }
-    
-    
-    // if no (false), ask question again by running fight() again
-//     else {
-//         fight();
-//     }
-    
-// }   else {
-//     window.alert("You need to choose a valid option. Try again!");
-// }
+        playerHealth = Math.max(0, playerHealth - damage);
+
+        // Log a resulting message to the console so we know that it worked. 
+        console.log(
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        );
+        
+        // check player's health
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!");
+            // leave while() loop if player is dead 
+            break;
+        } else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
     }
 };
 
@@ -103,7 +96,7 @@ var startGame = function() {
         // pick new enemy to fight based on the index of the enemyNames array.
         var pickedEnemyName = enemyNames[i];
         // reset enemyHealth before starting new fight
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60);
         // use debugger to pause script from running and check what's going on at that moment in the code.
         debugger;
         // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter.
@@ -191,6 +184,13 @@ var shop = function() {
             break;
     }
 };
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max-min + 1) + min);
+
+    return value;
+}
 
 // start the game when the page loads
 startGame();
